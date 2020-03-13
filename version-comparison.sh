feeb678edd1f91a37468e976a21a7db64e8643fc
Siddharth Lakhani <lakhanisiddharth_007@yahoo.co.in>
2020-03-13 12:32:02 +0000

    version-comparison file added

---
+++ version-comparison.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+new=7.90.0
+old=7.10.0
+
+if [ "$(printf '%s\n' "$new" "$old" | sort -V | head -n1)" = "$new" ]; then 
+        echo "updated with latest version"
+else
+        echo "need to update"
+fi
